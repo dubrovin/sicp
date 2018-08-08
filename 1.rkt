8bd7b49e239352f7936548b5cb5d9b5c8058d852
Dubrovin Ruslan <dubrovin.ruslan@gmail.com>
2018-08-08 23:30:12 +0300

    added exaples of 1.1.* chapter

---
+++ 1.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define (square x) (* x x))
+(square 21)
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(sum-of-squares 5 8)
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+(f 4)
